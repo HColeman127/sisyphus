662240b95cc7f5beea71b515f962cb24869bcc50
HColeman127 <harrymcoleman@gmail.com>
2019-01-14 17:57:55 -0800

    scoopity ehoop

--- asteroids-master/evolution.py
+++ asteroids-master/evolution.py
@@ -6,26 +6,28 @@ from FitnessWrapper import FitnessWrapper as fw
 
 
 # CONSTANTS
-POPULATION_SIZE = 10
+POPULATION_SIZE = 30
 NUMBER_OF_TRIALS = 10
 MAX_GENERATIONS = 1000
 
-SELECTION_PRESSURE_0 = 2
-MUTATION_RATE_0 = 0
-MUTATION_STRENGTH = 0.2
 CROSSOVER_WEIGHT = 0.25
+MUTATION_RATE_0 = 0.001
+MUTATION_STRENGTH_0 = 0.1
+SELECTION_PRESSURE_0 = 0
 
 
 def main():
     population = []
     next_generation = []
     best_individual = []
+    avg_fit_history = []
 
-    best_fitness = 0
+    best_fit_value = 0
     prev_gen_div = 0
     prev_fit_div = 0
 
     mutation_rate = MUTATION_RATE_0
+    mutation_strength = MUTATION_STRENGTH_0
     selection_pressure = SELECTION_PRESSURE_0
 
     # file names
@@ -42,10 +44,10 @@ def main():
     print("     NUMBER OF TRIALS: %4d" % NUMBER_OF_TRIALS)
     print("      MAX GENERATIONS: %4d\n" % MAX_GENERATIONS)
 
-    print("   SELECTION PRESSURE:", SELECTION_PRESSURE_0)
-    print("INITIAL MUTATION RATE:", MUTATION_RATE_0)
-    print("    MUTATION STRENGTH:", MUTATION_STRENGTH)
-    print("     CROSSOVER WEIGHT:", CROSSOVER_WEIGHT, "\n")
+    print("          CROSSOVER WEIGHT:", CROSSOVER_WEIGHT)
+    print("     INITIAL MUTATION RATE:", MUTATION_RATE_0)
+    print(" INITIAL MUTATION STRENGTH:", MUTATION_STRENGTH_0)
+    print("INITIAL SELECTION PRESSURE:", SELECTION_PRESSURE_0, "\n")
     print("--------------------------------------------------------")
     print("SEEDING POPULATION...")
     start_time = time.time()
@@ -55,65 +57,74 @@ def main():
     print("--------------------------------------------------------")
 
     # increment generations
-    for gen in range(MAX_GENERATIONS):
+    for generation in range(MAX_GENERATIONS):
 
-        print("GENERATION:", gen)
+        print("GENERATION:", generation)
         print("\nASSESSING POPULATION FITNESS...")
         start_time = time.time()
         fits = assess_gen_fits(population)
         print("FITNESS ASSESSED")
         print("ASSESSMENT TIME: %ds\n" % (time.time() - start_time))
 
+        # sort population by fitness
         ranked = rank_fit(population, fits)
         fits.sort()
 
+        # save average generation fitness to file
         f = open(filename, "a")
-        average = np.mean(fits)
-        f.write("%d\n" % average)
+        avg_fit = np.mean(fits)
+        f.write("%d\n" % avg_fit)
         f.close()
 
+        # find average fitness change in past 10 generations
+        avg_fit_history.append(avg_fit)
+        if len(avg_fit_history) > 10:
+            avg_fit_history.pop(0)
+
+        avg_fit_delta
+
+        # genetic diversity
         gen_div = gen_diversity(population)
         gen_delta = gen_div - prev_gen_div
         prev_gen_div = gen_div
 
+        # fitness diversity
         fit_div = math.sqrt(np.var(np.array(fits)))
         fit_delta = fit_div - prev_fit_div
         prev_fit_div = fit_div
 
-        print("  [GENERATION] AVERAGE FITNESS: %d" % average)
+        print("  [GENERATION] AVERAGE FITNESS: %d" % avg_fit)
         print("                 GEN DIVERSITY: %f" % gen_div)
         print("                 GEN DELTA: %f" % gen_delta)
         print("                 FIT DIVERSITY: %f" % fit_div)
         print("                 FIT DELTA: %f" % fit_delta)
 
-        if max(fits) > best_fitness:
+        if max(fits) > best_fit_value:
             best_individual = ranked[-1]
-            best_fitness = max(fits)
+            best_fit_value = max(fits)
             best_numpy = np.asarray(best_individual)
 
             f2 = open(filename2, "a")
             writable = np.array2string(best_numpy, separator=",", max_line_width=5000,
-                                       formatter={'float_kind': lambda x: "%f" % x})
-            f2.write(str(best_fitness)+"\n"+writable+"\n\n")
+                                       formatter={'float_kind': lambda x: "%f" % x})[1:-1]
+            f2.write(str(best_fit_value)+"\n"+writable+"\n\n")
             f2.close()
 
-            print("\n  [INDIVIDUAL] BEST FITNESS:", best_fitness)
+            print("\n  [INDIVIDUAL] BEST FITNESS:", best_fit_value)
             print("               GENOME:", best_individual)
-
-        # seed the next generation
-
-        next_generation.clear()
 
         print("\nSEEDING NEXT GENERATION...")
 
-        #mutation_rate = 0.005/gen_div
-        #selection_pressure = 1000/fit_div
+
+
 
         print("               MUTATION RATE: %f" % mutation_rate)
+        print("           MUTATION STRENGTH: %f" % mutation_strength)
         print("          SELECTION PRESSURE: %f" % selection_pressure)
 
         fit_dist = gen_probability_distribution(fits, selection_pressure)
 
+        next_generation.clear()
         for i in range(round(POPULATION_SIZE / 2)):
             parent_one = select_parent(ranked, fit_dist)
             parent_two = select_parent(ranked, fit_dist)
@@ -123,8 +134,8 @@ def main():
 
             child_one, child_two = crossover(parent_one, parent_two)
 
-            mutate(child_one, mutation_rate)
-            mutate(child_one, mutation_rate)
+            mutate(child_one, mutation_rate, mutation_strength)
+            mutate(child_one, mutation_rate, mutation_strength)
 
             next_generation.append(child_one)
             next_generation.append(child_two)
@@ -153,7 +164,11 @@ def select_parent(population, fit_dist):
 
 
 def gen_probability_distribution(fits, selection_pressure):
-    weighted_fits = [fit**selection_pressure for fit in fits]
+    pop_size = len(fits)
+    weighted_fits = []
+    for i in range(pop_size):
+        weighted_fits.append((1/(pop_size-i))**(1+selection_pressure))
+
     total_fitness = sum(weighted_fits)
 
     fit_percents = [fit/total_fitness for fit in weighted_fits]
@@ -219,7 +234,7 @@ def crossover(genome_one, genome_two):
     return child1, child2
 
 
-def mutate(genetic_code, mutation_rate, max_value=1, min_value=-1):
+def mutate(genetic_code, mutation_rate, mutation_strength, max_value=1, min_value=-1):
     """ accepts:    GENETIC_CODE input of an n length list of floats
                     MUTATION_RATE the chance any given float (gene) mutates.
                                 between zero and one (default is .05)
@@ -232,7 +247,7 @@ def mutate(genetic_code, mutation_rate, max_value=1, min_value=-1):
     for i in range(len(genetic_code)):
         if random.uniform(0, 1) <= mutation_rate:
             # assigns new value using normal distribution, staying in upper and lower limits
-            genetic_code[i] = max(min_value, min(max_value, random.gauss(genetic_code[i], MUTATION_STRENGTH)))
+            genetic_code[i] = max(min_value, min(max_value, random.gauss(genetic_code[i], mutation_strength)))
 
 
 # run main
